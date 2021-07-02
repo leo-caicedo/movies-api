ef266e00639313dd9959df612fa4235992906f71
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-07-02 18:09:34 -0500

    Capa de manejo de errores usando un middleware

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv-flow').config();
 
 const config = {
   dev: process.env.NODE_ENV !== 'production',
--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const app = express();
 
 const { config } = require('./config/index');
 const moviesAPI = require('./routes/movies');
+const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+
+// body parser
+app.use(express.json());
 
 moviesAPI(app);
+
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`);
