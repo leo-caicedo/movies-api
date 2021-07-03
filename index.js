1dc4bf4d6a3b5c087412444d019018f354010612
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-07-02 20:29:54 -0500

    Implementando Boom

--- index.js
+++ index.js
@@ -3,16 +3,29 @@ const app = express();
 
 const { config } = require('./config/index');
 const moviesAPI = require('./routes/movies');
-const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+const {
+  logErrors,
+  wrapErrors,
+  errorHandler,
+} = require('./utils/middleware/errorHandlers');
+
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 // body parser
 app.use(express.json());
 
+// routes
 moviesAPI(app);
 
+// Catch 404
+app.use(notFoundHandler);
+
+// Errors middleware
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, function () {
+  // eslint-disable-next-line no-console
   console.log(`Listening http://localhost:${config.port}`);
 });
